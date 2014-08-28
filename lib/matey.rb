217b858860ee8dceb15c08980b3af8df585795a5
brianthecoder <brian@20jeans.com>
2014-08-28 13:34:46 -0700

    cleanup

--- lib/matey.rb
+++ lib/matey.rb
@@ -1,17 +1,18 @@
 $:.unshift File.dirname(__FILE__)
-$:.unshift File.dirname("#{__FILE__}/../views")
 
 require 'dotenv'
 Dotenv.load
 
 require "matey/version"
 require "active_support/core_ext"
-require "active_support/core_ext/object/to_json"
+require "virtus"
 
 module Matey
-  # Your code goes here...
   autoload :Client, "matey/client"
   autoload :Request, "matey/request"
   autoload :Response, "matey/response"
-  autoload :Location, "matey/location"
+
+  # models
+  autoload :Location, "matey/models/location"
+  autoload :Package, "matey/models/package"
 end
--- lib/matey/request.rb
+++ lib/matey/request.rb
@@ -1,25 +1,24 @@
-require "active_model"
 require "tilt"
 
 module Matey
   class Request
-    include ActiveModel::Validations
-
-    DEFAULTS = {}
-
     attr_accessor :client
 
     def render(options = {})
       options.reverse_merge!(DEFAULTS)
-      template = Tilt.new("views#{self.class.name.gsub('Matey','').underscore}.slim")
+      template = Tilt.new("#{self.class.root}/#{self.class.name.gsub('Matey','').underscore}.slim")
       template.render(self, options: options)
     end
 
     protected
 
+    def self.root
+      "#{File.dirname(__FILE__)}/views"
+    end
+
     def partial(name, options = {})
       carrier = self.class.to_s.split('::')[1]
-      template = Tilt.new("views/#{carrier.underscore}/partials/_#{name}.slim")
+      template = Tilt.new("#{self.class.root}/#{carrier.underscore}/partials/_#{name}.slim")
       template.render(self, options)
     end
   end
